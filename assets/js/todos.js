cc371dfe2105944bdd37f715196c0b0db3442c00
z4ha <jakmusze@gmail.com>
2017-08-29 00:48:29 +0200

    First jQuery functions

---
+++ assets/js/todos.js
@@ -0,0 +1,19 @@
+// Check off specific teodos by clicking
+$('ul').on('click', 'li', function () {
+  $(this).toggleClass('completed')
+})
+
+$('ul').on('click', 'span', function (event) {
+  $(this).parent().fadeOut('500', function () {
+    $(this).remove()
+  })
+  event.preventDefault()
+})
+
+$("input[type='text']").keypress(function (event) {
+  if (event.which === 13) {
+    var todoText = $(this).val()
+    $(this).val('')
+    $('ul').append('<li><span>X</span> ' + todoText + '</li>')
+  }
+})
